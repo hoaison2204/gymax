378bf5841042d018a7de62d9abb8822ec57d9c10
Son Nguyen <hoaison2204@gmail.com>
2023-02-01 14:10:10 +0700

    final update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import "./App.scss";
 
+// banner ( 6 pictures in body)
 import Banner from "./Components/banner/Banner";
+// Other gyms
 import Brand from "./Components/brand/Brand";
-
+// Feedback from customers
 import Customer from "./Components/Customer/Customer";
+// Contact information
 import Footer from "./Components/footer/Footer";
-
+// Header navigation
 import Header from "./Components/header/Header";
+// Shop now
 import ShopNow from "./Components/shopNow/shopNow";
+// product list
 import ProductList from "./Components/Product/ProductList/ProductList";
 import "./index.scss";
 
