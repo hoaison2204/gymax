3ff7f31e32fab0928240bef0c38c8c2c77a37d44
Son Nguyen <hoaison2204@gmail.com>
2023-02-07 21:40:36 +0700

    edit

--- src/Components/shopNow/shopNow.js
+++ src/Components/shopNow/shopNow.js
@@ -7,16 +7,18 @@ function ShopNow() {
       <div className="shopNow-inner">
         <div className="container">
           <div className="shopNow-content">
-            <h1>Dumbbells Shop</h1>
-            <h3>Best body fitness</h3>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer
-            </p>
-            <a className="btn-shopNow" href="/#">
-              Shop now
-            </a>
+            <div className="shopNow-content-inner">
+              <h1>Dumbbells Shop</h1>
+              <h3>Best body fitness</h3>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text
+                ever since the 1500s, when an unknown printer
+              </p>
+              <a className="btn-shopNow" href="/#">
+                Shop now
+              </a>
+            </div>
           </div>
         </div>
       </div>
